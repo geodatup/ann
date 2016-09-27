02b8f55dbab788618fae2ff4167d784d3bc72c20
Hugo Roussaffa <hugoroussaffa@gmail.com>
2016-09-27 09:45:23 +0200

    rm useless comments

--- ann/urls.py
+++ ann/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls import *  # NOQA
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from django.http import HttpResponse
 
 admin.autodiscover()
 
@@ -17,7 +16,6 @@ urlpatterns = i18n_patterns('',
         {'sitemaps': {'cmspages': CMSSitemap}}),
     url(r'^select2/', include('django_select2.urls')),
     url(r'^', include('cms.urls')),
-    #url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
 )
 
 # This is only needed when using runserver.
